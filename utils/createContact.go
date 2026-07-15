3bdb0f9499d0acb175dbc5ca50d0a607bf92c0a0
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 23:03:10 +0800

    fix: use exec

--- utils/createContact.go
+++ utils/createContact.go
@@ -6,16 +6,18 @@ import (
 )
 
 func CreateContact(data database.Contact) bool {
-	conn, _ := Conn()
-	_, err := conn.Query(context.Background(), `
+	conn, er := Conn()
+	if er != nil {
+		return false
+	}
+	defer conn.Close(context.Background())
+	_, err := conn.Exec(context.Background(), `
 		INSERT INTO "contacts" ("First_name", "Last_name", "Email", "Phone") VALUES
-		($1, $2, $3, $4)
+		($1, $2, $3, $4);
 	`, data.First_name, data.Last_name, data.Email, data.Phone)
 
 	if err != nil {
 		return false
-	} else {
-		return true
 	}
-
+	return true
 }
