9aacc91cee710298504df678ca9045cd0afe1386
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 23:02:36 +0800

    fix: template home

--- pages/home.go
+++ pages/home.go
@@ -3,6 +3,7 @@ package pages
 import (
 	"fmt"
 	"koda-b8-db5/utils"
+	"os"
 	"strings"
 
 	"github.com/jackc/pgx/v5"
@@ -10,25 +11,30 @@ import (
 
 func Home(conn *pgx.Conn) {
 	for {
+		utils.Clear()
 		var input string
 		fmt.Printf("Management Contacts\n\n")
 		fmt.Printf("1. List All Contacts\n")
 		fmt.Printf("2. Add Contact\n")
 		fmt.Printf("3. Change Contact\n")
 		fmt.Printf("4. Delete Contact\n")
+		fmt.Printf("\n\n0. Exit\n")
 		fmt.Printf("\nChose a menu :  ")
 		fmt.Scanf("%s", &input)
 
 		switch strings.ToLower(input) {
 		case "1":
-			fmt.Printf("Dalam pengembangan\n\n")
-			utils.ListCOntacts(conn)
+			utils.ListContacts()
+			fmt.Printf("\n\nPress enter to back!  ")
+			fmt.Scanf("\n")
 		case "2":
-			fmt.Printf("Dalam pengembangan")
+			AddContact()
 		case "3":
-			fmt.Printf("Dalam pengembangan")
+			Change()
 		case "4":
-			fmt.Printf("Dalam pengembangan")
+			Delete()
+		case "0":
+			os.Exit(1)
 		default:
 			continue
 		}
