72e99c108b11c42b40837472a29ff0cd9138befe
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 23:42:08 +0800

    fix:  tyupo contact

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,4 +1,4 @@
 docker-entrypoint.sh postgres 
 env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /app/.env
 until pg_isready -q; do sleep 1; done
-exec /app/ontact-management
+exec /app/contact-management
