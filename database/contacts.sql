604f48786e2b92c078b0a31657ed1eee91a976eb
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 15:28:59 +0800

    feat: create table use sql

---
+++ database/contacts.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "contacts" (
+    "Id" bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "First_name" VARCHAR(40) NOT NULL,
+    "Last_name" VARCHAR(40) NOT NULL,
+    "Email" VARCHAR(40) NOT NULL,
+    "Phone" VARCHAR(13),
+    "CreatedAt" TIMESTAMP DEFAULT NOW(),
+    "UpdatedAt" TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO "contacts" ("First_name", "Last_name", "Email", "Phone") VALUES
+('Almaruf', 'Hidayat', 'almarufhidayat99@gmail.com', '082393468568');
+
+SELECT * FROM "contacts";
+
+DROP TABLE contacts;
