70a96240da814e9c59b571e7c4a03227b6ec5a04
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 17:11:27 +0800

    feat: pages add contact

---
+++ pages/addcontact.go
@@ -0,0 +1,36 @@
+package pages
+
+import (
+	"fmt"
+	"koda-b8-db5/database"
+	"koda-b8-db5/utils"
+)
+
+func AddContact() {
+	fmt.Printf("--[ ADD CONCTACT ]---\n\n")
+	var first string
+	var last string
+	var email string
+	var phone string
+	fmt.Printf("\nFirst Name : ")
+	fmt.Scanf("%s", &first)
+	fmt.Printf("\nLast Name  : ")
+	fmt.Scanf("%s", &last)
+	fmt.Printf("\nEmail      : ")
+	fmt.Scanf("%s", &email)
+	fmt.Printf("\nPhone      : ")
+	fmt.Scanf("%s", &phone)
+	newContact := database.Contact{
+		First_name: first,
+		Last_name:  last,
+		Email:      email,
+		Phone:      phone,
+	}
+
+	res := utils.CreateContact(newContact)
+	if res == true {
+		fmt.Printf("Add contact %s %s success\n\n", first, last)
+		fmt.Printf("Press Enter to back! ")
+		fmt.Scanf("\n")
+	}
+}
