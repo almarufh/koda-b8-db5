371a97d2358d058eae5e7cc31ec3806a226b8a80
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-15 16:17:17 +0800

    feat: gorutine for get data

--- utils/listContacts.go
+++ utils/listContacts.go
@@ -3,18 +3,45 @@ package utils
 import (
 	"fmt"
 	"koda-b8-db5/database"
+	"sync"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func ListCOntacts(conn *pgx.Conn) {
-	GetContacts(conn)
+	wg := sync.WaitGroup{}
+	signal := make(chan string)
+	wg.Add(1)
+	go GetContacts(conn, &wg, &signal)
 
-	for _, res := range *(database.Contacts()) {
-		fmt.Printf("ID    : %d\n", res.Id)
-		fmt.Printf("Name  : %s %s\n", res.First_name, res.Last_name)
-		fmt.Printf("Email : %s\n", res.Email)
-		fmt.Printf("Phone : %s\n", res.Phone)
-		fmt.Println()
+	i := ""
+	for i != "success" {
+		if <-signal == "success" {
+			i = "success"
+		}
 	}
+
+	wg.Wait()
+
+	contacts := database.Contacts()
+
+	if len(*contacts) < 1 {
+		fmt.Printf("Contacts empty\n\nPress enter to back! ")
+		fmt.Scanf("\n")
+		return
+	}
+
+	fmt.Printf("---[ List All COntacts ]---\n\n")
+
+	for _, res := range *contacts {
+		fmt.Printf("   -----------------------\n")
+		fmt.Printf("ID [%d]\n", res.Id)
+		fmt.Printf("   Name  : %s %s\n", res.First_name, res.Last_name)
+		fmt.Printf("   Email : %s\n", res.Email)
+		fmt.Printf("   Phone : %s\n", res.Phone)
+		fmt.Printf("   -----------------------\n\n")
+	}
+
+	fmt.Printf("\n\nPress enter to back!  ")
+	fmt.Scanf("\n")
 }
