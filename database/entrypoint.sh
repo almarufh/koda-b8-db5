c8f77beba313d38f570c993e2b9f6ff27eace170
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-16 08:51:48 +0800

    fix: for bug gotenv search DATABASE_URL get string kosong

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,9 +1,31 @@
+
 docker-entrypoint.sh postgres &
 
-env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /app/.env || true
+echo "DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB" > /app/.env
 
 until pg_isready -q -U postgres; do 
   sleep 1
 done
 
 exec /app/contact-management
+
+
+# docker-entrypoint.sh postgres &
+
+# env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /app/.env || true
+
+# until pg_isready -q -U postgres; do 
+#   sleep 1
+# done
+
+# exec /app/contact-management
+
+#!/bin/sh
+# docker-entrypoint.sh postgres &
+
+# env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /app/.env || true
+
+# until pg_isready -q -U postgres; do 
+#   sleep 1
+# done
+# exec /app/contact-management
